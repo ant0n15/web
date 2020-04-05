05562ff125db359992e7e4d3c00bd5c44dc4e216
ant0n15 <anthony.077@gmail.com>
2020-04-05 19:16:16 +0300

    First commit

---
+++ store/src/App.js
@@ -0,0 +1,54 @@
+//Author: Antonis Lamprou
+import React, { Component } from 'react';
+import Orders from './components/orders';
+class App extends Component {
+
+  constructor() {
+    super();
+  
+    // ... existing constructor code
+  
+    this.loadData = this.loadData.bind(this)
+  }
+  
+  state = {
+    data: [],
+    customer_data: [],
+    product_data: []
+  }
+  
+  loadData(){
+    fetch('https://aqueous-thicket-50958.herokuapp.com/api/v1/orders/1/pending')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ data })
+    })
+    .catch(console.log)
+
+    //TODO: customer id
+    fetch('https://aqueous-thicket-50958.herokuapp.com/api/v1/customers/1')
+    .then(res => res.json())
+    .then(customer_data => {
+      this.setState({ customer_data })
+    })
+
+    //TODO: category id
+    fetch('https://aqueous-thicket-50958.herokuapp.com/api/v1/products/1')
+    .then(res => res.json())
+    .then(product_data => {
+      this.setState({ product_data })
+    })
+  }
+
+  componentDidMount() {
+    setInterval(this.loadData, 4000); 
+  }
+
+  render() {
+    return (
+      <Orders product_data={this.state.product_data} customer_data={this.state.customer_data} data={this.state.data} />
+    )
+  }
+}
+
+export default App;
---
+++ store/src/components/orders.js
@@ -0,0 +1,37 @@
+//Author: Antonis Lamprou
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+
+const Orders = ({ data, customer_data, product_data }) => {
+    return (
+    <div>
+        <div className="header">Orders List</div>
+
+        {data.map((item) => (
+        
+        <div key={item.id + item._id} className="flexi">
+            <div key={item._id} className="card">
+                <div className="left">
+                    <div className="card-body">
+                        <h5 className="card-title">Order id: {item.id}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">Date/Time: {item.date}</h6>
+                        <p className="card-text">Address: {customer_data[0].address}</p>
+                        <p className="card-text">Status: {item.order_status}</p>
+                    </div>
+                </div>
+                <div className="right">
+                    <div><b>Products</b><br></br> {product_data[0].name} <br></br> {product_data[1].name}</div>
+                </div>
+
+                <Button className="btn" variant="contained" color="primary">Confirm</Button>
+                <div width='10px'><font color="#F0FFFF">...</font></div>
+                <Button className="btn" variant="contained" color="secondary">Cancel</Button>
+            </div>
+        </div>
+        ))}
+    </div>
+    )
+};
+
+export default Orders
